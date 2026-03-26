b242e857b71837342d687a1b394213613cbf0bee
Dauren <d.k.nurgaziev@gmail.com>
2026-03-26 19:53:28 +0500

    i dont know what i've updated but commit

--- users/views.py
+++ users/views.py
@@ -1,18 +1,9 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect, messages
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 
 def auth_page(request):
     return render(request, 'auth.html')
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            new_user = form.save()
-            messages.info(request, "Thanks for registering. You are now logged in.")
-            new_user = authenticate(username=form.cleaned_data['username'],
-                                    password=form.cleaned_data['password1'],
-                                    )
-            login(request, new_user)
-            return HttpResponseRedirect("/dashboard/")
+def join_page(request):
+    return render(request, 'register.html')
